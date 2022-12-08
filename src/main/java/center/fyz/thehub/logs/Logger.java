eb9a73c5ca9a7bb1d86b6493d5e15a9b5fcc56d4
Fyz <35635613+FyzRDC@users.noreply.github.com>
2022-12-08 21:32:31 +0100

    Update Logger.java

--- src/main/java/center/fyz/thehub/logs/Logger.java
+++ src/main/java/center/fyz/thehub/logs/Logger.java
@@ -1,8 +1,22 @@
 package center.fyz.thehub.logs;
 
+import center.fyz.thehub.users.User;
+
 public class Logger {
 
     public static void log(String message) {
+        try {
+            new Log(LogType.CONSOLE, message);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void log(LogType type, String message, User user) {
+        try {
+            new Log(type, message, user);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
